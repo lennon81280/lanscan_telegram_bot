92e94eb532353fb3f1ed31751665457d31212589
lennon81280 <lennon81280@gmail.com>
2017-10-18 16:04:20 +0330

    Create scanlan.py

---
+++ scanlan.py
@@ -0,0 +1,31 @@
+from telegram.ext import Updater, CommandHandler
+import logging
+import nmap
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s ' )
+
+def start(bot, update):
+    update.message.reply_text('Hello World!')
+
+def scan(bot, update):
+
+	nm = nmap.PortScanner()
+	machines = nm.scan(hosts='192.168.1.0/24', arguments='-sP')
+	for k,v in machines['scan'].iteritems(): 
+		if str(v['status']['state']) == 'up':
+			#print "-------"
+			try:
+				a=str(v['addresses']['ipv4'])+"-"+str(v['addresses']['mac'])+ "-"+str(v['vendor'])
+				print(a)
+				update.message.reply_text(a)
+			except: 
+				update.message.reply_text(str(v['addresses']['ipv4'])+"-No MAC detected.")
+
+
+updater = Updater('YourTokenHERE')
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('scan', scan))
+
+updater.start_polling()
+updater.idle()
