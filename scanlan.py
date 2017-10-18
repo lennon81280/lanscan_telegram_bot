e21b7ea8257599f3f343bae20ad61c7f35d283bd
lennon81280 <lennon81280@gmail.com>
2017-10-18 16:05:36 +0330

    Update scanlan.py

--- scanlan.py
+++ scanlan.py
@@ -13,15 +13,11 @@ def scan(bot, update):
 	machines = nm.scan(hosts='192.168.1.0/24', arguments='-sP')
 	for k,v in machines['scan'].iteritems(): 
 		if str(v['status']['state']) == 'up':
-			#print "-------"
 			try:
-				a=str(v['addresses']['ipv4'])+"-"+str(v['addresses']['mac'])+ "-"+str(v['vendor'])
-				print(a)
-				update.message.reply_text(a)
+				update.message.reply_text(str(v['addresses']['ipv4'])+"-"+str(v['addresses']['mac'])+ "-"+str(v['vendor']))
 			except: 
 				update.message.reply_text(str(v['addresses']['ipv4'])+"-No MAC detected.")
-
-
+				
 updater = Updater('YourTokenHERE')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
